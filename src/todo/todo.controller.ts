807c101824a0542d33e82cef63d0912a8f354a51
khunnunthawat <khunnunthawat@hotmail.com>
2021-05-06 19:04:53 +0700

    nest-js

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { TodoService } from './todo.service';
+
+@Controller('todo')
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+  @Get()
+  getTodos() {
+    return this.todoService.getTodos();
+  }
+  @Post()
+  postTodo(@Body('title') title: string, @Body('subtitle') subtitle: string) {
+    this.todoService.addTodo(title, subtitle);
+  }
+  @Delete('/:id')
+  deleteTodoById(@Param('id') id: string) {
+    console.log(`id: ${id}`);
+    return this.todoService.removeTodoById(id);
+  }
+  // @Patch('/:id')
+  // updateTodoById(@Param('id') id: string, @Param('title') title: string) {
+  //   console.log(`id: ${id}, title: ${title}`);
+  //   return `Update id is ${id}`;
+  // }
+}
